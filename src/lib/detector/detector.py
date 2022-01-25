1a50116f072f13738b80cc055502c11a2f1a4267
luca-zanella-dvl <lzanella@fbk.eu>
2022-01-25 16:29:40 +0100

    Second video anonymization release

---
+++ src/lib/detector/detector.py
@@ -0,0 +1,83 @@
+import torch
+import cv2
+import numpy as np
+
+from models.model import create_model, load_model
+from datasets.dataset import letterbox
+from utils.general import check_img_size, non_max_suppression, scale_coords, xywh2xyxy, xyxy2xywh
+
+
+class Detector(object):
+    def __init__(self, opt, model_path, imgsz, device):
+        print("Creating model...")
+        self.opt = opt
+        self.model_path = model_path
+        self.device = device
+        self.model = create_model(opt.arch, model_path)
+        self.model = load_model(self.model, model_path)
+        self.model = self.model.to(device)
+        self.model.eval()
+
+        self.stride = int(self.model.stride.max())
+        self.imgsz = check_img_size(imgsz, s=self.stride)  # check image size
+
+    def pre_process(self, im0):
+        # Padded resize
+        im = letterbox(im0, self.imgsz, stride=self.stride)[0]
+
+        # Convert
+        im = im.transpose((2, 0, 1))[::-1]  # HWC to CHW, BGR to RGB
+        im = np.ascontiguousarray(im)
+
+        im = torch.from_numpy(im).to(self.device)
+        im = im.float()  # uint8 to fp32
+        im /= 255  # 0 - 255 to 0.0 - 1.0
+        if len(im.shape) == 3:
+            im = im[None]  # expand for batch dim
+        return im
+
+    def post_process(self, pred, classes):
+        # NMS
+        pred = non_max_suppression(pred, classes=classes)
+        return pred
+
+    def process_im(self, im0, classes):
+        """
+        Takes a single frame as input, and scores the frame using yolo5 model.
+        :param frame: input frame in numpy/list/tuple format.
+        :return: Labels and Coordinates of objects detected by model in the frame.
+        """
+        im = self.pre_process(im0.copy())
+
+        # Inference
+        pred = self.model(im)[0]
+        pred = self.post_process(pred, classes)
+
+        results = []
+
+        # Process predictions
+        for i, det in enumerate(pred):  # per image
+            # gn = torch.tensor(im0_shape)[[1, 0, 1, 0]]  # normalization gain whwh
+            if len(det):
+                # Rescale boxes from img_size to im0 size
+                det[:, :4] = scale_coords(im.shape[2:], det[:, :4], im0.shape).round()
+
+                for *xyxy, conf, cls in reversed(det):
+                    # xywh = (xyxy2xywh(torch.tensor(xyxy).view(1, 4)) / gn).view(-1).tolist()  # normalized xywh
+                    results.append((self.cls2label(cls), *xyxy, conf))  # label format
+
+        return results
+
+    def cls2label(self, cls):
+        """
+        For a given class, return corresponding string label.
+        :param x: numeric label
+        :return: corresponding string label
+        """
+        c = int(cls)  # integer class
+        label = self.model.names[c]
+        return label
+
+    def count_label(self, results, label):
+        n = len([cls == label for cls, xywh, conf in results])
+        return n
---
+++ src/lib/opts.py
@@ -0,0 +1,159 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import argparse
+import os
+import sys
+from pathlib import Path
+
+from utils.general import increment_path
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent.parent
+
+
+def extant_file(fname):
+    """
+    'Type' for argparse - checks that file exists but does not open.
+    """
+    if not os.path.exists(fname):
+        raise argparse.ArgumentTypeError(f"{fname} does not exist")
+    return fname
+
+
+class opts(object):
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        # object detection
+        self.parser.add_argument(
+            "--arch",
+            default="yolo",
+            help="model architecture. Currently only supports yolo",
+        )
+
+        # head detection
+        self.parser.add_argument(
+            "--head-model",
+            type=extant_file,
+            default=get_project_root() / 'weights/crowdhuman1280x_yolov5x6.pt',
+            help="head detection model.pt path",
+        )
+        self.parser.add_argument(
+            "--head-imgsz",
+            "--head-img",
+            "--head-img-size",
+            nargs="+",
+            type=int,
+            default=[1280],
+            help="inference size h,w",
+        )
+        self.parser.add_argument(
+            "--head-classes",
+            nargs="+",
+            type=int,
+            help="filter by class: --classes 0, or --classes 0 2 3",
+        )
+
+        # vehicle detection
+        self.parser.add_argument(
+            "--veh-model",
+            type=extant_file,
+            default=get_project_root() / 'weights/yolov5x6.pt',
+            help="vehicle detection model.pt path",
+        )
+        self.parser.add_argument(
+            "--veh-imgsz",
+            "--veh-img",
+            "--veh-img-size",
+            nargs="+",
+            type=int,
+            default=[1280],
+            help="inference size h,w",
+        )
+        self.parser.add_argument(
+            "--veh-classes",
+            nargs="+",
+            type=int,
+            help="filter by class: --classes 0, or --classes 0 2 3",
+        )
+        
+        self.parser.add_argument(
+            "--lpd-model",
+            type=extant_file,
+            default=get_project_root() / 'weights/lpd_yolov5x.pt',
+            help="license plate detection model.pt path",
+        )
+        self.parser.add_argument(
+            "--lpd-imgsz",
+            "--lpd-img",
+            "--lpd-img-size",
+            nargs="+",
+            type=int,
+            default=[640],
+            help="inference size h,w",
+        )
+        self.parser.add_argument(
+            "--lpd-classes",
+            nargs="+",
+            type=int,
+            help="filter by class: --class 0, or --class 0 2 3",
+        )
+
+        # system
+        self.parser.add_argument(
+            "--device", default="", help="cuda device, i.e. 0 or 0,1,2,3 or cpu"
+        )
+        self.parser.add_argument(
+            "--num_workers",
+            type=int,
+            default=8,
+            help="dataloader threads. 0 for single-thread.",
+        )
+        self.parser.add_argument(
+            "--seed", type=int, default=317, help="random seed"
+        )  # from CornerNet
+
+        # IO
+        self.parser.add_argument(
+            "--source",
+            type=extant_file,
+            required=True,
+            help="file/dir/URL/glob",
+        )
+        self.parser.add_argument(
+            "--project",
+            default=get_project_root() / "runs/anonymize",
+            help="save results to project/name",
+        )
+        self.parser.add_argument(
+            "--name", default="exp", help="save results to project/name"
+        )
+        self.parser.add_argument(
+            "--exist-ok",
+            action="store_true",
+            help="existing project/name ok, do not increment",
+        )
+
+    def parse(self, args=""):
+        if args == "":
+            opt = self.parser.parse_args()
+        else:
+            opt = self.parser.parse_args(args)
+
+        opt.head_imgsz *= 2 if len(opt.head_imgsz) == 1 else 1  # expand
+        opt.veh_imgsz *= 2 if len(opt.veh_imgsz) == 1 else 1  # expand
+        opt.lpd_imgsz *= 2 if len(opt.lpd_imgsz) == 1 else 1  # expand
+
+        opt.save_dir = increment_path(
+            Path(opt.project) / opt.name, exist_ok=opt.exist_ok
+        )  # increment run
+        opt.save_dir.mkdir(parents=True, exist_ok=True)  # make dir
+        print("The output will be saved to ", opt.save_dir)
+
+        return opt
+
+    def init(self, args=""):
+        opt = self.parse(args)
+        return opt
